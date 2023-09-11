e64904bf96bcea9e3757ebd306c6b299131a381b
Ang <angyuze6@gmail.com>
2023-09-11 20:49:13 +0800

    Added login page and connected with database

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import QuestionsPage from "./pages/QuestionsPage/QuestionsPage";
 import RegisterPage from "./pages/RegisterPage/RegisterPage";
+import LoginPage from "./pages/LoginPage/LoginPage";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Routes>
         <Route path="/questions" element={<QuestionsPage />} />
         <Route path="/register" element={<RegisterPage />} />
+        <Route path="/login" element={<LoginPage />} />
         {/* Add more routes as needed */}
       </Routes>
     </Router>
---
+++ frontend/src/pages/LoginPage/LoginPage.tsx
@@ -0,0 +1,10 @@
+import LoginForm from "./components/LoginForm";
+
+export default function LoginPage() {
+  // TODO replace with useContext
+  return (
+    <div>
+      <LoginForm />
+    </div>
+  );
+}
