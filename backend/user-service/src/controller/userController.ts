f97d6edd2c2a4db95338f87555541906d11e67e8
Bransonlj <Bransonlj@gmail.com>
2023-09-14 16:48:53 +0800

    fix registerUser, format styling with prettier

--- backend/user-service/src/controller/userController.ts
+++ backend/user-service/src/controller/userController.ts
@@ -1,7 +1,7 @@
-import { Request, Response } from 'express';
-import { userFunctions } from '../model/userModel';
-import * as bcrypt from 'bcrypt';
-import { v4 as uuidv4 } from 'uuid';
+import { Request, Response } from "express";
+import { AccountType, User, userFunctions } from "../model/userModel";
+import * as bcrypt from "bcrypt";
+import { v4 as uuidv4 } from "uuid";
 
 /**
  * Creates a new user
@@ -10,36 +10,40 @@ import { v4 as uuidv4 } from 'uuid';
  * @param res Outgoing HTTP response indicating success of user creation
  */
 export const register = async (req: Request, res: Response) => {
-    try {
-      // Parameters for users table
-        const user_id = uuidv4();
-        const username = req.body.username;
-        const email = req.body.email;
-        const password = req.body.password;
-        // TODO: Not sure how future implementation of admin vs user role. By default is user role
-        const account_type = (await userFunctions.getIdForUserAccountType()) || 'null';
+  try {
+    // Parameters for users table
+    const user_id = uuidv4();
+    const username = req.body.username;
+    const email = req.body.email;
+    const password = req.body.password;
 
-        const emailTaken = await userFunctions.isEmailTaken(email);
+    // TODO: Not sure how future implementation of admin vs user role. By default is user role
+    const account_type: AccountType = "User";
 
-        if (emailTaken) {
-            return res.status(400).json({ message: 'That email is already in use' });
-        }
+    // TODO: should only email be unique or username as well
+    const emailTaken = await userFunctions.isEmailTaken(email);
 
-        const hashed_pw = await bcrypt.hash(password, 8);
-        const user = {
-            user_id,
-            account_type,
-            username,
-            email,
-            hashed_pw
-        }
-
-        const userInsertResult = await userFunctions.insertUser(user);
-        return res.status(200).json({ message: 'User registered' });
-    } catch (error) {
-        console.log(error);
-        return res.status(500).json({ message: 'An error occurred while registering user' });
+    if (emailTaken) {
+      return res.status(400).json({ message: "That email is already in use" });
     }
+
+    const hashed_pw = await bcrypt.hash(password, 8);
+    const user: User = {
+      user_id,
+      account_type,
+      username,
+      email,
+      hashed_pw,
+    };
+
+    const userInsertResult = await userFunctions.insertUser(user);
+    return res.status(200).json({ message: "User registered" });
+  } catch (error) {
+    console.log(error);
+    return res
+      .status(500)
+      .json({ message: "An error occurred while registering user" });
+  }
 };
 
 /**
@@ -49,91 +53,103 @@ export const register = async (req: Request, res: Response) => {
  * @param res Outgoing HTTP response indicating success of user login
  */
 export const login = async (req: Request, res: Response) => {
-    try {
-        const { email, password } = req.body;
-        const user = await userFunctions.getUserByEmail(email);
+  try {
+    const { email, password } = req.body;
+    const user = await userFunctions.getUserByEmail(email);
 
-        if (!user) {
-            return res.status(400).json({ message: 'Email or password is wrong.' });
-        }
-
-        const passwordMatch = await bcrypt.compare(password, user.hashed_pw);
-
-        if (passwordMatch) {
-            // TODO: Generate session token for user
-            
-            res.status(200).json({ user_id: user.user_id });
-        } else {
-            res.status(400).json({ message: 'Invalid Password' });
-        }
-    } catch (error) {
-        console.log(error);
-        res.status(500).json({ message: 'An error occurred while logging in.' });
+    if (!user) {
+      return res.status(400).json({ message: "Invalid email" });
     }
+
+    const passwordMatch = await bcrypt.compare(password, user.hashed_pw);
+
+    if (passwordMatch) {
+      // TODO: Generate session token for user
+
+      res.status(200).json({ user_id: user.user_id });
+    } else {
+      res.status(400).json({ message: "Invalid Password" });
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "An error occurred while logging in." });
+  }
 };
 
 export const getProfile = async (req: Request, res: Response) => {
-    try {
-        const { id: user_id } = req.params;
-        const user = await userFunctions.getUserById(user_id);
+  try {
+    const { id: user_id } = req.params;
+    const user = await userFunctions.getUserById(user_id);
 
-        if (!user) {
-            return res.status(400).json({ message: 'User profile does not exists.' });
-        }
-
-        const username = user.username;
-        const email = user.email;
-        const hashed_pw = user.hashed_pw;
-
-        //res.status(200).json({ message: 'Successfully retrieved profile!!!'});
-        res.status(200).json({ username: username, email: email, hashedPassword: hashed_pw });
-
-    } catch (error) {
-        console.log(error);
-        res.status(500).json({ message: 'An error occurred while retreiving profile.' });
+    if (!user) {
+      return res.status(400).json({ message: "User profile does not exists." });
     }
+
+    // TODO: should we pass hashed_pw
+    const username = user.username;
+    const email = user.email;
+    const hashed_pw = user.hashed_pw;
+
+    res
+      .status(200)
+      .json({ username: username, email: email, hashedPassword: hashed_pw });
+  } catch (error) {
+    console.log(error);
+    res
+      .status(500)
+      .json({ message: "An error occurred while retreiving profile." });
+  }
 };
 
 export const deleteProfile = async (req: Request, res: Response) => {
-    const { id: user_id } = req.params;
+  const { id: user_id } = req.params;
 
-    try {
-        const result = await userFunctions.deleteUserById(user_id);
+  try {
+    const result = await userFunctions.deleteUserById(user_id);
 
-        res.status(201).json({message: 'Successfully deleted profile'});
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({ message: 'An error occurred while deleting the profile.' });
-    }
+    res.status(201).json({ message: "Successfully deleted profile" });
+  } catch (error) {
+    console.error(error);
+    res
+      .status(500)
+      .json({ message: "An error occurred while deleting the profile." });
+  }
 };
 
 export const updateProfile = async (req: Request, res: Response) => {
-    const { id: user_id } = req.params;
-    const { email, username } = req.body;
+  const { id: user_id } = req.params;
+  const { email, username } = req.body;
 
-    try {
-        const result = await userFunctions.updateUserProfile(user_id, email, username);
+  try {
+    const result = await userFunctions.updateUserProfile(
+      user_id,
+      email,
+      username
+    );
 
-        return res.status(201).json({ message: 'Profile updated successfully' });
-    } catch (error) {
-        console.error('Error updating profile:', error);
-        return res.status(500).json({ message: 'Failed to update profile: ', error });
-    }
+    return res.status(201).json({ message: "Profile updated successfully" });
+  } catch (error) {
+    console.error("Error updating profile:", error);
+    return res
+      .status(500)
+      .json({ message: "Failed to update profile: ", error });
+  }
 };
 
 export const changePassword = async (req: Request, res: Response) => {
-    const { id: user_id } = req.params;
-    const { oldPassword, newPassword } = req.body;
+  const { id: user_id } = req.params;
+  const { oldPassword, newPassword } = req.body;
 
-    try {
-        const result = await userFunctions.changeUserPassword(user_id, oldPassword, newPassword);
+  try {
+    const result = await userFunctions.changeUserPassword(
+      user_id,
+      oldPassword,
+      newPassword
+    );
 
-        return res.status(201).json({ message: 'Password changed successfully' });
-    } catch (error) {
-        console.error('Error updating profile:', error);
-        return res.status(500).json({ message: error});
-    }
-
-}
-
-
+    return res.status(201).json({ message: "Password changed successfully" });
+  } catch (error) {
+    console.error("Error updating profile:", error);
+    return res.status(500).json({ message: error });
+  }
+};
