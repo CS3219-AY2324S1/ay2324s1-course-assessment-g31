dc17a181ff4c568c7614223127e269c86221cf9f
John Benedict Yan <johnbenedictyan@gmail.com>
2023-10-12 16:20:27 +0800

    Update matching control panel page with new styling

--- frontend/src/pages/MatchingControlPanelPage/MatchingControlPanelPage.tsx
+++ frontend/src/pages/MatchingControlPanelPage/MatchingControlPanelPage.tsx
@@ -11,6 +11,14 @@ interface MatchingRequest {
   success: boolean;
 }
 
+interface Matching {
+  id: number;
+  user1Id: string;
+  user2Id: string;
+  requestId: number;
+  dateTimeMatched: Date;
+}
+
 function SingleQueue({
   title,
   requests,
@@ -41,10 +49,43 @@ function SingleQueue({
             </li>
           ))
         ) : (
-          <p className="text-center text-gray-500">No one in queue</p>
+          <p className="bg-gray-100 text-center text-gray-500 p-4 rounded-md shadow-md">
+            No one in queue
+          </p>
         )}
       </ul>
     </div>
+  );
+}
+
+function MatchingList({ matchings }: { matchings: Matching[] }) {
+  return (
+    <ul className="divide-y divide-gray-100">
+      {matchings.length > 0 ? (
+        matchings.map((matching) => (
+          <li
+            key={`${matching.user1Id}-${matching.user2Id}-${matching.requestId}`}
+            className="bg-gray-100 flex justify-between gap-x-6 p-3 rounded-md border shadow-md mb-2"
+          >
+            <div className="flex min-w-0 gap-x-4">
+              <div className="min-w-0 flex-auto">
+                <p className="text-sm font-semibold leading-6 text-gray-900">
+                  {matching.user1Id} successfully matched with{" "}
+                  {matching.user2Id}
+                </p>
+                <p className="mt-1 truncate text-xs leading-5 text-gray-500">
+                  {matching.dateTimeMatched.toString()}
+                </p>
+              </div>
+            </div>
+          </li>
+        ))
+      ) : (
+        <p className="bg-gray-100 text-center text-gray-500">
+          No successful matchings
+        </p>
+      )}
+    </ul>
   );
 }
 
@@ -60,6 +101,8 @@ function MatchingControlPanelPage() {
     [],
   );
 
+  const [matchings, setMatchings] = useState<Matching[]>([]);
+
   const matchingController = new MatchingController();
 
   useEffect(() => {
@@ -69,23 +112,51 @@ function MatchingControlPanelPage() {
   }, []);
 
   useEffect(() => {
+    matchingController.getMatchings().then((res) => {
+      setMatchings(res);
+    });
+  }, []);
+
+  useEffect(() => {
     setEasyQueueRequests(
-      requests.filter((request) => request.difficulty === "easy"),
+      requests.filter(
+        (request) => request.difficulty === "easy" && !request.success,
+      ),
     );
     setMediumQueueRequests(
-      requests.filter((request) => request.difficulty === "medium"),
+      requests.filter(
+        (request) => request.difficulty === "medium" && !request.success,
+      ),
     );
     setHardQueueRequests(
-      requests.filter((request) => request.difficulty === "hard"),
+      requests.filter(
+        (request) => request.difficulty === "hard" && !request.success,
+      ),
     );
   }, [requests]);
 
   return (
     <PageContainer>
-      <div className="flex flex-row w-full justify-around">
-        <SingleQueue title="Easy Queue" requests={easyQueueRequests} />
-        <SingleQueue title="Medium Queue" requests={mediumQueueRequests} />
-        <SingleQueue title="Hard Queue" requests={hardQueueRequests} />
+      <div className="flex flex-col gap-5">
+        <div className="bg-slate-300 p-5">
+          <h1 className="text-xl font-bold mb-5 text-center">
+            Active Matching Requests
+          </h1>
+          <div className="flex flex-row w-full justify-around">
+            <SingleQueue title="Easy Queue" requests={easyQueueRequests} />
+            <SingleQueue title="Medium Queue" requests={mediumQueueRequests} />
+            <SingleQueue title="Hard Queue" requests={hardQueueRequests} />
+          </div>
+        </div>
+
+        <div className="bg-slate-300 p-5">
+          <h1 className="text-xl font-bold mb-5 text-center">
+            Successful Matching
+          </h1>
+          <div className="flex flex-row w-full justify-around">
+            <MatchingList matchings={matchings} />
+          </div>
+        </div>
       </div>
     </PageContainer>
   );
