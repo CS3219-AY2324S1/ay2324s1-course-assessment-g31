f4dd000efcce9bd852d720c85c6f38dcbcd369a5
Ang <angyuze6@gmail.com>
2023-11-11 00:56:24 +0800

    Cleaned up unused files

--- backend/chat-service/src/events/topics/matching/matching.ts
+++
@@ -1,8 +0,0 @@
-enum MatchingTopics {
-  CREATE = "matching-created",
-  UPDATE = "matching-updated",
-  DELETE = "matching-deleted",
-  FAIL = "matching-failed",
-}
-
-export default MatchingTopics;
