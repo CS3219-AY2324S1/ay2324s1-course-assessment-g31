34fc8d8dd9aac45a0aa9d4199d01cffddf7853fc
John Benedict Yan <johnbenedictyan@gmail.com>
2023-11-10 16:21:44 +0800

    fix layout

--- frontend/src/components/container/Component.tsx
+++ frontend/src/components/container/Component.tsx
@@ -2,7 +2,7 @@ import { IContainerProps } from "./Container.interface";
 
 function ComponentContainer({ children }: IContainerProps) {
   return (
-    <div className="bg-gray-100">
+    <div className="bg-gray-100 dark:bg-gray-800 py-16 sm:py-24">
       <div className="mx-auto max-w-7xl px-6 lg:px-8">{children}</div>
     </div>
   );
---
+++ frontend/src/pages/Users/SignInPage/__tests__/index.spec.tsx
@@ -0,0 +1,13 @@
+import { expect, test } from "@playwright/experimental-ct-react";
+
+import { HooksConfig } from "../../../../../playwright";
+import SignInPage from "..";
+
+test.use({ viewport: { width: 500, height: 500 } });
+
+test("Should Render", async ({ page, mount }) => {
+  const component = await mount<HooksConfig>(<SignInPage />, {
+    hooksConfig: { enableRouting: true },
+  });
+  await expect(component).toContainText("Sign in to your account");
+});
