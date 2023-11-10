9fd88e4ec760158d92ddc690edecb1a074ac89e8
John Benedict Yan <johnbenedictyan@gmail.com>
2023-11-10 14:33:19 +0800

    add new contexts

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,26 @@
 import { AuthProvider } from "./context/AuthContext";
+import { CollaborationProvider } from "./context/CollaborationContext";
+import { DarkModeProvider } from "./context/DarkModeContext";
 import { MatchingProvider } from "./context/MatchingContext";
+import { NotificationProvider } from "./context/NotificationContext";
+import { QuestionProvider } from "./context/QuestionContext";
 import MainRouter from "./router/main";
 
 // ES added a default
 export default function App() {
   return (
-    <AuthProvider>
-      <MatchingProvider>
-        <MainRouter />
-      </MatchingProvider>
-    </AuthProvider>
+    <NotificationProvider>
+      <AuthProvider>
+        <MatchingProvider>
+          <DarkModeProvider>
+            <QuestionProvider>
+              <CollaborationProvider>
+                <MainRouter />
+              </CollaborationProvider>
+            </QuestionProvider>
+          </DarkModeProvider>
+        </MatchingProvider>
+      </AuthProvider>
+    </NotificationProvider>
   );
 }
