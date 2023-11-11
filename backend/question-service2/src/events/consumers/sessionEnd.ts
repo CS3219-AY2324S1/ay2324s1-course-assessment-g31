cce6084cc4474e29977260abfabb3d38434a9f4c
John Benedict Yan <johnbenedictyan@gmail.com>
2023-11-11 15:26:53 +0800

    update retrofit question service with new relations

--- backend/question-service2/src/events/consumers/sessionEnd.ts
+++ backend/question-service2/src/events/consumers/sessionEnd.ts
@@ -3,6 +3,7 @@ import prismaClient from "../../util/prisma/client";
 import { ConsumerFunction } from "./main.interface";
 
 const questionService = new QuestionService(prismaClient);
+
 export const sessionEndConsumer: ConsumerFunction = (message) => {
   if (message.value) {
     const { questionId } = JSON.parse(message.value.toString());
