04fe418609cb195736011106d148b9cefa3b6298
Branson Lam <80864566+Bransonlj@users.noreply.github.com>
2023-11-13 19:18:24 +0800

    Update history service to user Parser, Service, Controller

--- backend/history-service/src/server.ts
+++ backend/history-service/src/server.ts
@@ -1,8 +1,12 @@
 import express, { Express } from "express";
 import dotenv from "dotenv";
-import router from "./routes/routes";
 import cors from "cors";
 import EventConsumer from "./events/consumer/main";
+import HistoryService from "./services/history/history.service";
+import prismaClient from "./util/prisma/client";
+import HistoryParser from "./parsers/history/history.parser";
+import HistoryController from "./controllers/history/history.controller";
+import HistoryRouter from "./routers/history/router";
 
 dotenv.config();
 
@@ -14,10 +18,24 @@ const corsOptions = {
   optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
 };
 
+// Services
+const historyService = new HistoryService(prismaClient);
+
+// Parsers
+const historyParser = new HistoryParser();
+
+// Controllers
+const historyController = new HistoryController(historyService, historyParser);
+
+const historyRouter = new HistoryRouter(historyController, express.Router());
+
 app.use(cors(corsOptions));
 app.use(express.json());
 
-app.use("/", router);
+app.use("/api/healthCheck", (_req, res) => {
+  res.send("OK");
+});
+app.use("/api/history", historyRouter.registerRoutes());
 
 app.listen(port, () => {
   console.log(
