32690f12660628a15ef14e273f8581cfa61a28ec
Sampy147 <samuelpangc@gmail.com>
2023-11-14 15:10:33 +0800

    Updated error messages in Register page for consistency and navlink in Hero component

--- frontend/src/components/Hero.tsx
+++ frontend/src/components/Hero.tsx
@@ -9,21 +9,21 @@ function Hero() {
           Collaborate. Code. Conquer.
         </h1>
         <p className="mt-6 text-lg leading-8 text-gray-600 dark:text-gray-400">
-          Master coding interviews together with our collaborative platform.
+          Master coding interviews together now with our collaborative platform.
         </p>
         <div className="mt-10 flex items-center justify-center gap-x-6">
           <a
-            href="/get-started"
+            href="/sign-in"
             className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
           >
-            Get started
+            Get started now
           </a>
-          <a
+          {/* <a
             href="/learn-more"
             className="text-sm font-semibold leading-6 text-gray-900 dark:text-gray-100"
           >
             Learn more <span aria-hidden="true">→</span>
-          </a>
+          </a> */}
         </div>
       </div>
       <div className="mt-16 flow-root sm:mt-24">
