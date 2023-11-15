d939bed31a1712613f5f9714961a92c34fd82519
John Benedict Yan <johnbenedictyan@gmail.com>
2023-11-15 11:51:46 +0800

    delete unused pages

--- frontend/src/router/main.tsx
+++ frontend/src/router/main.tsx
@@ -1,24 +1,22 @@
-import { User } from "@firebase/auth";
-import React, { useContext } from "react";
-import { Route, Routes, useNavigate } from "react-router-dom";
+import { User } from '@firebase/auth';
+import React, { useContext } from 'react';
+import { Route, Routes, useNavigate } from 'react-router-dom';
 
-import { useAuth } from "../context/AuthContext";
-import { NotificationContext } from "../context/NotificationContext";
-import ForbiddenPage from "../pages/ForbiddenPage/ForbiddenPage";
-import LandingPage from "../pages/LandingPage";
-import Layout from "../pages/Layout/Layout";
-import MatchingControlPanelPage from "../pages/MatchingControlPanelPage/MatchingControlPanelPage";
-import MatchPage from "../pages/MatchPage";
-import NotFoundPage from "../pages/NotFoundPage";
-import ProfilePage from "../pages/ProfilePage/ProfilePage";
-import QuestionForm from "../pages/QuestionForm/QuestionForm";
-import AllQuestionPage from "../pages/Questions/AllQuestionsPage";
-import SingleQuestionPage from "../pages/Questions/SingleQuestionPage";
-import CreateSingleQuestionPage from "../pages/Questions/SingleQuestionPage/create";
-import EditSingleQuestionPage from "../pages/Questions/SingleQuestionPage/edit";
-import SolutionForm from "../pages/SolutionForm/SolutionForm";
-import RegistrationPage from "../pages/Users/RegistrationPage";
-import SignInPage from "../pages/Users/SignInPage";
+import { useAuth } from '../context/AuthContext';
+import { NotificationContext } from '../context/NotificationContext';
+import ForbiddenPage from '../pages/ForbiddenPage/ForbiddenPage';
+import LandingPage from '../pages/LandingPage';
+import Layout from '../pages/Layout/Layout';
+import MatchingControlPanelPage from '../pages/MatchingControlPanelPage/MatchingControlPanelPage';
+import MatchPage from '../pages/MatchPage';
+import NotFoundPage from '../pages/NotFoundPage';
+import ProfilePage from '../pages/ProfilePage/ProfilePage';
+import AllQuestionPage from '../pages/Questions/AllQuestionsPage';
+import SingleQuestionPage from '../pages/Questions/SingleQuestionPage';
+import CreateSingleQuestionPage from '../pages/Questions/SingleQuestionPage/create';
+import EditSingleQuestionPage from '../pages/Questions/SingleQuestionPage/edit';
+import RegistrationPage from '../pages/Users/RegistrationPage';
+import SignInPage from '../pages/Users/SignInPage';
 
 // import RegisterPage from "./pages/RegisterPage/RegisterPage";
 // import LoginPage from "./pages/LoginPage/LoginPage";
@@ -133,24 +131,6 @@ export default function MainRouter() {
         />
 
         <Route
-          path="/questions/form"
-          element={
-            <ProtectedRoute user={currentUser} rolesNeeded={["user"]}>
-              <QuestionForm />
-            </ProtectedRoute>
-          }
-        />
-
-        <Route
-          path="/questions/solution/form"
-          element={
-            <ProtectedRoute user={currentUser} rolesNeeded={["user"]}>
-              <SolutionForm />
-            </ProtectedRoute>
-          }
-        />
-
-        <Route
           path="/profile"
           element={
             <ProtectedRoute user={currentUser} rolesNeeded={["user"]}>
@@ -164,15 +144,6 @@ export default function MainRouter() {
           element={
             <ProtectedRoute user={currentUser} rolesNeeded={["user"]}>
               <MatchPage />
-            </ProtectedRoute>
-          }
-        />
-
-        <Route
-          path="/questions/:id"
-          element={
-            <ProtectedRoute user={currentUser} rolesNeeded={["user"]}>
-              <SingleQuestionPage />
             </ProtectedRoute>
           }
         />
