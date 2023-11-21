9626e510b56a64cd3ac19ffcd6a89619351b9521
Joshua Chiang <96032660+ChickenChiang@users.noreply.github.com>
2023-11-21 22:25:05 +0800

    Edit create.ts to fix matching issues

--- backend/matching-service/src/events/consumers/matchingRequest/create.ts
+++ backend/matching-service/src/events/consumers/matchingRequest/create.ts
@@ -1,33 +1,32 @@
-import { MatchingRequest } from "@prisma/client";
+import { MatchingRequest } from '@prisma/client';
+import { Partitioners } from 'kafkajs';
 
-import { Matching } from "../../../interfaces/matching/object";
-import MatchingService from "../../../services/matching/matching.service";
-import MatchingRequestService from "../../../services/matchingRequest/matchingRequest.service";
-import prismaClient from "../../../util/prisma/client";
-import MatchingProducer from "../../producers/matching/producer";
-import MatchingRequestProducer from "../../producers/matchingRequest/producer";
-import { ConsumerFunction } from "../main.interface";
-import kafka from "../../kafka";
-import logger from "../../../util/logger";
-import { Partitioners } from "kafkajs";
+import MatchingService from '../../../services/matching/matching.service';
+import MatchingRequestService from '../../../services/matchingRequest/matchingRequest.service';
+import logger from '../../../util/logger';
+import prismaClient from '../../../util/prisma/client';
+import kafka from '../../kafka';
+import MatchingProducer from '../../producers/matching/producer';
+import MatchingRequestProducer from '../../producers/matchingRequest/producer';
+import { ConsumerFunction } from '../main.interface';
 
 const matchingEventProducer = new MatchingProducer(
-  kafka.producer({ createPartitioner: Partitioners.LegacyPartitioner }),
+  kafka.producer({ createPartitioner: Partitioners.LegacyPartitioner })
 );
 const matchingRequestEventProducer = new MatchingRequestProducer(
-  kafka.producer({ createPartitioner: Partitioners.LegacyPartitioner }),
+  kafka.producer({ createPartitioner: Partitioners.LegacyPartitioner })
 );
 const matchingService = new MatchingService(prismaClient);
 const matchingRequestService = new MatchingRequestService(prismaClient);
 
 const createMatchingRequestConsumer: ConsumerFunction = async (message) => {
   logger.info(
-    "WE HAVE RECEIVED A MESSAGE FOR THE CREATION OF A MATCHING REQUEST",
+    "WE HAVE RECEIVED A MESSAGE FOR THE CREATION OF A MATCHING REQUEST"
   );
   if (message.value) {
     // Parse the json message
     const inputMatchingReq: MatchingRequest = JSON.parse(
-      message.value.toString(),
+      message.value.toString()
     );
 
     const matchReqFromDB: MatchingRequest | null =
@@ -45,26 +44,50 @@ const createMatchingRequestConsumer: ConsumerFunction = async (message) => {
       return;
     }
 
-    const matching: Matching = await matchingService.create({
-      user1Id: counterPartyMatchReq.userId,
-      user2Id: matchReqFromDB.userId,
-      requestId: matchReqFromDB.id,
-      difficulty: matchReqFromDB.difficulty,
-      questionIdRequested: matchReqFromDB.questionId,
+    let matching;
+
+    await prismaClient.$transaction(async (prisma) => {
+      // Step 1: Begin a transaction
+      await prisma.$executeRaw`BEGIN`;
+
+      try {
+        // Step 2: Lock the row for client 1
+        await prisma.$executeRaw`SELECT * FROM "MatchingRequest" WHERE id = ${counterPartyMatchReq.id} FOR UPDATE`;
+        await prisma.$executeRaw`SELECT * FROM "MatchingRequest" WHERE id = ${matchReqFromDB.id} FOR UPDATE`;
+
+        // Step 3: Update the value for client 1
+        await prisma.matchingRequest.update({
+          where: { id: counterPartyMatchReq.id },
+          data: { success: true },
+        });
+
+        await prisma.matchingRequest.update({
+          where: { id: matchReqFromDB.id },
+          data: { success: true },
+        });
+
+        matching = await prisma.matching.create({
+          data: {
+            user1Id: counterPartyMatchReq.userId,
+            user2Id: matchReqFromDB.userId,
+            requestId: matchReqFromDB.id,
+            difficulty: matchReqFromDB.difficulty,
+            questionIdRequested: matchReqFromDB.questionId,
+          },
+        });
+
+        // Step 4: Commit the transaction
+        await prisma.$executeRaw`COMMIT`;
+      } catch (error) {
+        // Step 5: Rollback the transaction if an error occurs
+        await prisma.$executeRaw`ROLLBACK`;
+        console.log(error);
+      }
     });
 
-    // Update matching request
-    await matchingRequestService.update(counterPartyMatchReq.id, {
-      ...counterPartyMatchReq,
-      success: true,
-    });
-
-    await matchingRequestService.update(matchReqFromDB.id, {
-      ...matchReqFromDB,
-      success: true,
-    });
-
-    matchingEventProducer.create(matching);
+    if (matching) {
+      matchingEventProducer.create(matching);
+    }
   }
 
   return Promise.resolve();
